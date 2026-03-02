7e4fb7d0b9e400b2d7973b4d42ee3c5f5e35ea8c
Márcio Júnior <marcio.mj70@gmail.com>
2026-03-01 23:05:25 -0300

    refactor: simplify onlyOwner modifier

--- fund-me/FundMe.sol
+++ fund-me/FundMe.sol
@@ -43,9 +43,7 @@ contract FundMe {
 
     modifier onlyOwner() {
         // require(msg.sender == i_owner, "Sender is not owner!");
-        if (msg.sender != i_owner) {
-            revert NotOwner();
-        }
+        if (msg.sender != i_owner) revert NotOwner();
         _;
     }
 
